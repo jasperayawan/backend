4c08bd71c545682b311bfba8eca7232261e0e64b
jasperayawan <ejayawan22@gmail.com>
2023-07-20 19:26:11 +0800

    initial commit

---
+++ server.js
@@ -0,0 +1,79 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const ParseServer = require('parse-server').ParseServer;
+const ParseDashboard = require('parse-dashboard');
+const cors = require('cors');
+const dotenv = require('dotenv')
+const app = express();
+
+
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
+app.use(cors());
+
+dotenv.config();
+
+const port = process.env.PORT
+
+
+// Initialize Parse Serve
+
+const api = new ParseServer({
+    databaseURI: "mongodb+srv://emlend:Hanoi123@emlend.zrknnqr.mongodb.net/",
+    appId: "123",
+    masterKey: "1234",
+    serverURL: "http://localhost:1337/parse",
+})
+
+const dashboardConfig = new ParseDashboard({
+    apps: [
+        {
+            appId: "123",
+            masterKey: "1234",
+            serverURL: "http://localhost:1337/parse",
+            appName: "emlend"
+        }
+    ]
+});
+
+api.start()
+
+app.use('/parse', api.app)
+app.use('/dashboard', dashboardConfig)
+
+// Handle Login request
+app.post('/login', async (req,res) => {
+    const { identifier, password } = req.body;
+    try{    
+        const user = await Parse.User.logIn(identifier, password);
+
+        res.json(user);
+    }
+    catch(err){
+        res.status(400).json({ error: err.message })
+    }
+})
+
+app.post('/register', async (req,res) => {
+    const { email, username, password } = req.body;
+    const user = new Parse.User();
+
+    user.set("email", email);
+    user.set('username', username);
+    user.set('password', password);
+
+    try{
+        await user.signUp();
+
+        res.json(user);
+    }
+    catch(err){
+        res.status(400).json({ error: err.message });;
+    }
+})
+
+
+app.listen(1337, () => {
+    console.log('Server is Listening to http://localhost:1337');
+
+})
